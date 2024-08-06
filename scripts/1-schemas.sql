63ddedc5f14896aa4639607791f9bddca9e3f028
kdywan_254049 <idywan_254049@e-science.pl>
2024-08-07 00:04:23 +0200

    Small changes - better columns names, new important columns e.g. number of sub comments in comment table, constraints names and indexes for most used columns

--- scripts/1-schemas.sql
+++ scripts/1-schemas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS USERS (
     user_id UUID PRIMARY KEY,
-    user_account_id varchar(50) NOT NULL UNIQUE,
-    nickname varchar(255) UNIQUE,
+    account_id varchar(50) NOT NULL,
+    nickname varchar(255),
     firstname varchar(50),
     surname varchar(50),
     tel varchar(15),
@@ -10,38 +10,60 @@ CREATE TABLE IF NOT EXISTS USERS (
     description TEXT,
     is_verified BOOLEAN NOT NULL DEFAULT FALSE,
     is_private BOOLEAN NOT NULL DEFAULT FALSE,
-    followers INT NOT NULL DEFAULT 0,
-    followings INT NOT NULL DEFAULT 0,
-    number_of_posts INT NOT NULL DEFAULT 0
+    followers_count INT NOT NULL DEFAULT 0 CHECK (followers_count > 0),
+    followings_count INT NOT NULL DEFAULT 0 CHECK (followings_count > 0),
+    posts_count INT NOT NULL DEFAULT 0 CHECK (posts_count > 0),
+    CONSTRAINT uq_users_account_id UNIQUE (account_id),
+    CONSTRAINT uq_users_nickname UNIQUE (nickname)
 );
 
 CREATE TABLE IF NOT EXISTS POSTS (
     post_id UUID PRIMARY KEY,
-    user_id UUID NOT NULL,
+    author_id UUID NOT NULL,
     creation_datetime TIMESTAMP WITH TIME ZONE NOT NULL,
-    description TEXT NOT NULL,
-    img BYTEA NOT NULL,
+    description TEXT,
+    content BYTEA NOT NULL,
     are_hidden_likes BOOLEAN NOT NULL DEFAULT FALSE,
     are_disabled_comments BOOLEAN NOT NULL DEFAULT FALSE,
-    CONSTRAINT fk_posts_user FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+    likes_count INT NOT NULL DEFAULT 0 CHECK (likes_count > 0),
+    CONSTRAINT fk_posts_author FOREIGN KEY (author_id) REFERENCES USERS(user_id)
 );
+
+CREATE IF NOT EXISTS INDEX ix_posts_author_id ON POSTS (author_id);
+CREATE IF NOT EXISTS INDEX ix_posts_creation_datetime ON POSTS (creation_datetime);
+
 
 CREATE TABLE IF NOT EXISTS POSTS_LIKES(
     post_like_id UUID PRIMARY KEY,
-    user_id UUID NOT NULL,
+    author_id UUID NOT NULL,
     post_id UUID NOT NULL,
-    CONSTRAINT fk_posts_likes_user FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+    CONSTRAINT fk_posts_likes_author FOREIGN KEY (author_id) REFERENCES USERS(user_id),
     CONSTRAINT fk_posts_likes_post FOREIGN KEY (post_id) REFERENCES POSTS(post_id)
 );
 
 CREATE TABLE IF NOT EXISTS COMMENTS (
     comment_id UUID PRIMARY KEY,
     post_id UUID NOT NULL,
-    user_id UUID NOT NULL,
+    author_id UUID NOT NULL,
     parent_comment_id UUID,
     content TEXT NOT NULL,
     creation_datetime TIMESTAMP WITH TIME ZONE NOT NULL,
+    likes_count INT NOT NULL DEFAULT 0 CHECK (likes_count >= 0),
+    sub_comments_count INT NOT NULL DEFAULT 0 CHECK (sub_comments_count >= 0),
     CONSTRAINT fk_comments_post FOREIGN KEY (post_id) REFERENCES POSTS(post_id),
-    CONSTRAINT fk_comments_user FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+    CONSTRAINT fk_comments_author FOREIGN KEY (author_id) REFERENCES USERS(user_id),
     CONSTRAINT fk_comments_parent_comment FOREIGN KEY (parent_comment_id) REFERENCES COMMENTS(comment_id)
 );
+
+CREATE IF NOT EXISTS ix_comments_parent_comment_id ON COMMENTS (parent_comment_id);
+CREATE IF NOT EXISTS INDEX ix_comments_creation_datetime ON COMMENTS (creation_datetime);
+
+
+CREATE TABLE IF NOT EXISTS COMMENTS_LIKES (
+    comment_like_id UUID PRIMARY KEY,
+    comment_id UUID NOT NULL,
+    author_id UUID NOT NULL,
+    CONSTRAINT fk_comments_likes_comment FOREIGN KEY (comment_id) REFERENCES COMMENTS(comment_id),
+    CONSTRAINT fk_comments_likes_author FOREIGN KEY (author_id) REFERENCES USERS(user_id)
+);
+
