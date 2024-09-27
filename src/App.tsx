cc4851ec32b3b69d993a2af6331c5c1cba6b2942
kdywan_254049 <idywan_254049@e-science.pl>
2024-09-27 20:04:44 +0200

    Implemented fetching messages on stomp

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Client } from '@stomp/stompjs';
+import { Message } from './models/Message';
 
 function App() {
+
+  const [messages, setMessages] = useState<Message[]>([])
+  
+  useEffect(() => {
+
+    const stompClient = new Client({
+      brokerURL: 'ws://localhost:9300/ws',
+      onConnect: () => {
+
+        stompClient.subscribe(
+          `/queue/3`, 
+          message => {
+            console.log(`Received message ${message.body}`)
+
+            const rawMessage = message.body
+            const convertedMessage: Message = JSON.parse(rawMessage)
+
+            setMessages((state) => [...state, convertedMessage])
+          },
+        )
+      }
+    })
+
+    window.addEventListener("beforeunload", (e) => {  
+      e.preventDefault();
+
+      stompClient.deactivate()
+    });
+
+    stompClient.activate()
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {messages.map((message: Message) => (
+        <div key={message.id}>{message.content}</div>
+      ))}
     </div>
   );
 }
