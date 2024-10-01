23f713653d57f7449a34eb2481cbfd4b92bd9bc5
kdywan_254049 <idywan_254049@e-science.pl>
2024-10-01 22:23:56 +0200

    Last?

--- src/pages/EmptyChat.tsx
+++ src/pages/EmptyChat.tsx
@@ -3,7 +3,12 @@ const EmptyChat = () => {
     const chatUrl = process.env.REACT_APP_CHAT_URL
 
     return (
-        <iframe src={chatUrl} height="100%" width="100%" sandbox="allow-popups allow-scripts allow-same-origin allow-forms">
+        <iframe 
+            src={`${window.location.origin}/${chatUrl}`} 
+            height="100%" 
+            width="100%" 
+            sandbox="allow-popups allow-scripts allow-same-origin allow-forms"
+        >
         </iframe>
     )
 }
